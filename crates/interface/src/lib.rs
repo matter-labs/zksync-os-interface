9b8339cfd17b87e82917718e7be75f92c46f4c21
Daniyar Itegulov <ditegulov@gmail.com>
2025-09-10 15:01:45 +1000

    tmp

--- crates/interface/src/lib.rs
+++ crates/interface/src/lib.rs
@@ -1,3 +1,6 @@
-pub mod error;
 pub mod traits;
-pub mod types;
+// Re-export types as a local submodule
+pub use zksync_os_types as types;
+// Re-export errors as a local submodule
+pub use zksync_os_types::error as error;
+pub mod output;
